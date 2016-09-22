ac42f1f88cf8b675a8a25803ca61ec5f222d9bad
wdeqin <wdeqin@gmail.com>
2016-09-22 23:38:58 +0800

    add nodejs helloworld sample

---
+++ js/helloworld.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(
+        function(request, response) {
+            console.log(request);
+            response.end("Hello, world!")
+        }).listen(8082)
